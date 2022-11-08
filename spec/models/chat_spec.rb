e4666e32a42c6f47eea49cfa27d5df441ce393ec
Mohamed Abd El-Meged <mabdelmeged.se@gmail.com>
2022-11-08 17:15:27 +0200

    fix chat_spec

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
   let!(:application) { create :application }
-  let!(:counter) { 0 }
-  let!(:chat) { create :chat, application_token: application.token, application: application, number: counter + 1 }
+  let!(:chat) { create :chat, application_token: application.token, application: application, number: 1 }
 
   it 'message count should be 0 and chat number should be 1' do
     expect(chat.messages_count).to eq(0)
-    expect(chat.number).to eq(counter)
+    expect(chat.number).to eq(1)
   end
 end
